8befa320afaf386803e49a899e9a6826f2fef410
Nick Lucius <nicholas.lucius2@cityofchicago.org>
2017-08-04 13:33:12 -0500

    Change license from text to link

--- App.R
+++ App.R
@@ -509,17 +509,10 @@ ui <- fixedPage(
            )
                   ),
          
-         fixedRow(
-           column(12, align="center",
-                  tags$h4("__________________________________________________________________________________________________________________________________________________")
-           ),
            column(12,
-                  tags$h5("Copyright (c) 2017 Renel Chesak and the City of Chicago. Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.")
-           ),
-           column(12, align="center",
-                  tags$h4("__________________________________________________________________________________________________________________________________________________")
+                  tags$h5(tags$a(href = "https://github.com/Chicago/Chicago-Beaches/blob/master/LICENSE", "View License"))
            )
-                  )
+                  
          )
 
 
